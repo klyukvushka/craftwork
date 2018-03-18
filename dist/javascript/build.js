ba3cec06fa593d95baadfbf8c319365e631d0964
forwardomg <forwardomg@gmail.com>
2018-03-18 13:27:43 +0200

    review slider

---
+++ dist/javascript/build.js
@@ -0,0 +1,10 @@
+  $('.slider').slick({
+    autoplay: false,
+    autoplaySpeed: 2500,
+    centerMode: true,
+    centerPadding: '25px',
+    slidesToShow: 3,
+    arrows: true,
+    prevArrow: ".arrow-prev",
+    nextArrow: ".arrow-next"
+  });
